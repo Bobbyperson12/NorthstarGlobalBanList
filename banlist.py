ec930df11afc45831f2ec932c3fa5cc740f3e8b6
Bobbyperson12 <bobbyperson12345@gmail.com>
2022-02-23 17:07:47 -0500

    general improvements

--- banlist.py
+++ banlist.py
@@ -1,13 +1,16 @@
 import requests
 import os
 
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
 url = "http://140.82.61.159/banlist.txt"
 # ban list
 
 r = requests.get(url)
 #retrieving data from the URL using get method
 
-with open("temp.txt", 'wb') as f:
+with open(os.path.join(__location__, "temp.txt"), 'wb') as f:
 #giving a name and saving it in any required format
 #opening the file in write mode
 
@@ -16,7 +19,7 @@ with open("temp.txt", 'wb') as f:
 #writes the URL contents from the server
 
 # open the data file
-file = open("temp.txt")
+file = open(os.path.join(__location__, "temp.txt"))
 # read the file as a list
 data = file.readlines()
 # close the file
@@ -31,10 +34,10 @@ for ban in data:
 #print(data) #debug
 #print(newdata) #debug
 
-with open('banlist.txt') as f:
+with open(os.path.join(__location__, "banlist.txt")) as f:
     file_lines = [line.rstrip() for line in f.readlines()] # spooky code that does spooky things to make this work
 
-with open("banlist.txt", "a+") as file: 
+with open(os.path.join(__location__, "banlist.txt"), "a+") as file: 
     for item in newdata: 
         if item not in file_lines:
             file.write(item + "\n")
@@ -42,4 +45,4 @@ with open("banlist.txt", "a+") as file:
         else:
             print("already banned: " + item)
 
-os.remove("temp.txt") # cleanup
+os.remove(os.path.join(__location__, "temp.txt")) # cleanup
